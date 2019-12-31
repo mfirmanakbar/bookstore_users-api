4b53635af1bef184e93232c743c753618c131442
mfirmanakbar <em.firman.akbar@gmail.com>
2019-12-31 21:37:28 +0700

    GH2 - Create domain & service for User - handling error when create user - manual handling using ioutil.ReadAll and Unmarshal

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,35 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/mfirmanakbar/bookstore_users-api/domain/users"
+	"github.com/mfirmanakbar/bookstore_users-api/services"
+	"io/ioutil"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement CreateUser")
+	var user users.User
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		//TODO : Handle error
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		//TODO : Handle json error
+		fmt.Println(err.Error())
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO : Handle user creation error
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {

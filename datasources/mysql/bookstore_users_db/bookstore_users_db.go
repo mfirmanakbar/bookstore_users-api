ce211100e4b22ef69079f6a965ca7e6584467fb8
mfirmanakbar <em.firman.akbar@gmail.com>
2020-01-02 21:56:25 +0700

    GH-6 Implement MySQL Database - create users table manually - implement save user by golang / insert query

---
+++ datasources/mysql/bookstore_users_db/bookstore_users_db.go
@@ -0,0 +1,45 @@
+package bookstore_users_db
+
+// the `_` on --> `_ "github.com/go-sql-driver/mysql"` it means we are not using but still need to import
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host     = "mysql_users_host"
+	mysql_users_schema   = "mysql_users_schema"
+)
+
+// open the mysql connection just 1 time
+var (
+	Client *sql.DB
+
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
+)
+
+func init() {
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@tcp(%s)/%s?charset=utf8",
+		username, password, host, schema,
+	)
+
+	var err error
+	Client, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = Client.Ping(); err != nil {
+		panic(err)
+	}
+	log.Println("database successfully configured")
+}
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,8 +2,15 @@ package users
 
 import (
 	"fmt"
+	"github.com/mfirmanakbar/bookstore_users-api/datasources/mysql/bookstore_users_db"
 	"github.com/mfirmanakbar/bookstore_users-api/utils/date_utils"
 	"github.com/mfirmanakbar/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, created_at) VALUES (?, ?, ?, ?);"
 )
 
 var (
@@ -11,6 +18,10 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := bookstore_users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
@@ -23,17 +34,31 @@ func (user *User) Get() *errors.RestErr {
 	return nil
 }
 
-func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exist", user.Id))
+// #1. the purpose of pointer as * is to make us able to modified this user object
+// #2. Defer statements are generally used to ensure that the files are closed when your work is finished with them,
+// 	   or to close the channel, or to catch the panics in the program.
+// #3. email_UNIQUE --> from http 500, when we save user with same email, then I custom to http 400
+func (user *User) Save() *errors.RestErr { // #1
+	stmt, err := bookstore_users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close() // #2
 
 	user.CreatedAt = date_utils.GetNowString()
 
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.CreatedAt)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) { // #3
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exist", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+	user.Id = userId
 	return nil
 }
